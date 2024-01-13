72a9af861bfea4602a8eee9f78191753eff829ef
Steevomzii <156409817+Steevomzii@users.noreply.github.com>
2024-01-13 10:18:43 +0300

    Update light_5.js

--- light_5.js
+++ light_5.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs').promises;
 const { SingleBar, Presets } = require('cli-progress');
 const fetchLighthouseReport = async (url, apiKey, progressBar, Queue) => {
-    const apiEndpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=mobile&category=performance&category=accessibility&category=best-practices&category=seo&key=${apiKey}`;
+    const apiEndpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(url)}&strategy=mobile&category=performance&category=accessibility&category=best-practices&category=seo`;
     try {
         const response = await axios.get(apiEndpoint);
         const data = response.data;
